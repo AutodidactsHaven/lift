db22af88b81ecf86a8f662e60017ce5cdddcb764
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2023-02-09 22:26:17 +1100

    demo of building dep. graph

---
+++ sample/player.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "magic.h"
+
+typedef struct player {
+    spell_tag quick_bar_spells[4];
+} player;
--- src/lift/dependency.py
+++ src/lift/dependency.py
@@ -1,17 +1,24 @@
+import os
+import re
 
-# TODO: use a custom class for more metadata tracking?
-class Node:
-    def __init__(self):
-        pass
+class FileNode:
+    def __init__(self, absolute_path, filename):
+        self.absolute_path = absolute_path
+        self.filename = filename
+        
+    def __repr__(self) -> str:
+        return f'{self.absolute_path}'
 
 class DependencyGraph:
     def __init__(self):
         self.num_of_nodes = 0
+        self.name_to_node_map = {}
         self.graph = {}
 
-    def add_files(self, filepaths):
-        for filepath in filepaths:
-            self.graph[filepath] = set()
+    def add_nodes(self, nodes):
+        for node in nodes:
+            self.name_to_node_map[node.filename] = node
+            self.graph[node] = set()
 
     """if directional is true then node1 DEPENDS ON node2 but not other way around"""
     def add_dependency(self, node1, node2, directional=False):
@@ -20,14 +27,54 @@ class DependencyGraph:
         if not directional:
             self.graph[node2].add(node1)
 
+    def get_node_by_filename(self, filename):
+        if self.name_to_node_map[filename]:
+            return self.name_to_node_map[filename]
+        else:
+            return None
+    
+    def __str__(self) -> str:
+        print_string = "Dependency Graph:\n"
+        for node in self.graph:
+            print_string += f'  {node.absolute_path} depends on [{self.graph[node]}]\n'
+        return print_string
+
+def extract_includes(text):
+    # the regular expression pattern to match "#include"
+    pattern = re.compile(r'#include\s+[<"](.*?)[>"]')
+
+    # find all matches in the input text
+    matches = re.findall(pattern, text)
+
+    # return the list of matches
+    return matches
+
 if __name__ == "__main__":
     from files import FILES
-    files = FILES("sample").get_files_with_exensions({".h",".c"})
-    print(files)
-    dgraph = DependencyGraph().add_files(files)
-    for file in files:
-        print("Handling ", file)
-        # 1. parse file for #include s
-        # 2. resolve relative paths to absolute paths
-        # 3. for each include:
-        #       add_dependency(...)
+
+    header_files = FILES("sample").get_files_with_exensions({".h"})
+    header_file_nodes = [ FileNode(p, os.path.basename(p)) for p in header_files]
+
+    source_files = FILES("sample").get_files_with_exensions({".c"})
+
+    dgraph = DependencyGraph()
+    dgraph.add_nodes(header_file_nodes)
+
+    # Let's deal with header files first
+    for file in header_file_nodes:
+        print("Handling ", file.absolute_path, file.filename)
+        # parse file for #include s
+        f = open(file.absolute_path, "r")
+        file_contents = f.read()
+        includes = extract_includes(file_contents)
+        
+        # for each include add_dependency
+        for include in includes:
+            target_node = dgraph.get_node_by_filename(include)
+            if target_node:
+                dgraph.add_dependency(file, target_node, directional=False) # header file includes are bidirectional
+            else:
+                print("couldnt find %s", target_node)
+        
+    print(dgraph)
+            
