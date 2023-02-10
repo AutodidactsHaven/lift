257abcbff11365b55b761cc3e274a3ee36a26f9c
Mihails M <mail@del.cx>
2023-02-10 21:30:52 +0000

    wip

--- sample/lift_build.py
+++ sample/lift_build.py
@@ -4,6 +4,7 @@ import sys
 import lift.print_color as Out
 from lift.files import Files
 from lift.compiler import Compiler
+from lift.workers import Workers
 
 # Toggle on/off printing of out.print_debug
 Out.TOGGLE_DEBUG = True 
@@ -23,16 +24,34 @@ def build(mode):
     Out.print_debug("Running build()")
 
     ### Finding files
+    Out.print_info("> Finding source files")
     # get all files in path_src
     path_src_files = Files(project_settings["path_src"])
     # exclude files which are not *.h and *.c 
-    pach_src_source = path_src_files.get_files_with_exensions({".h",".c"})
-    Out.print_debug(pach_src_source)
+    path_src_source = path_src_files.get_files_with_exensions({".h",".c"})
+    Out.print_debug(path_src_source)
 
     ### Compiler setup
+    Out.print_info("> Compiler setup")
     comp = Compiler(project_settings["compiler"])
     flags = comp.generate_flags(mode)
-    Out.print_debug(flags)
+
+    ### Dependancy graph
+    # TODO
+    
+    ### Generating object files
+    Out.print_info("> Generating *.o files")
+    workers_gen_o = Workers()
+    jobs = ["--version"]
+    for file in path_src_source:
+        if not file.endswith(".c"):
+            continue
+        working_dir = project_settings["working_dir"]
+        o_file_name = file.split("/")[-1].replace(".c", ".o")
+        o_file = f"{working_dir}/build/{o_file_name}"
+        jobs.append(f"{flags} -c '{file}' -o '{o_file}'")
+    which_compiler = project_settings["compiler"].lower()
+    workers_gen_o.work(which_compiler, jobs)
 
 
 def run():
--- src/lift/workers.py
+++ src/lift/workers.py
@@ -1,16 +1,36 @@
 import lift.print_color as Out
 
+import os
+import subprocess
+import shlex
 from threading import Thread
 import time
 
-def worker(command):
-    output, error = worker_run(command)
-    if output:
-        Out.print_normal(output)
-    if error:
-        Out.print_error(error)
+class Workers:
+    def internal_worker_capture(self, app, command):
+        output, error = self.internal_worker_exe(app, command)
+        if output:
+            Out.print_normal(output)
+        if error:
+            Out.print_error(error)
 
-def worker_run(app, args_str):
-    args = shlex.split(args_str)
-    result = subprocess.run([app] + [args], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    return result.stdout, result.stderr
+    def internal_worker_exe(self, app, args_str):
+        args = shlex.split(args_str)
+        result = subprocess.run([app] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return result.stdout, result.stderr
+
+    threads = []
+    def work(self, app, jobs_list):
+        Out.print_info(f"> - Total {len(jobs_list)} jobs")
+        for job in jobs_list:
+            t = Thread(target=self.internal_worker_capture, args=(app, job))
+            self.threads.append(t)
+            t.start()
+
+        Out.print_info("> - Waiting on all jobs to be done")
+        done = 0
+        for t in self.threads:
+            t.join()
+            done += 1
+            Out.print_info(f"> - Done {done}/{len(jobs_list)}")
+        Out.print_info("> - All workers are done")
