afca75e31f9db503233e0816a876f724ab976683
Mihails M <mail@del.cx>
2023-02-10 06:39:43 +0000

    Build plan in comments

--- src/lift/build.py
+++ src/lift/build.py
@@ -2,6 +2,16 @@ import lift.print as out
 
 class BUILD:
     def build():
+        # Figure out which files we need to build
+            # find all source files
+            # find all .o files
+            # compare timestamps
+        # Build dependency graph for files we need to build
+        # Generate compiler commands for making .o files
+        # Feed compiler commands into threading system
+        # Link .o into executable
+
+        
         out.print_info("Building the application");
 
     def run():
