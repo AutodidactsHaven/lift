778020376e2601df5ab9809fe1e0d8aa5db6ee7f
Mihails M <mail@del.cx>
2023-02-12 11:54:31 +0000

    new vision for lift_build.py

--- sample/lift_build.py
+++ sample/lift_build.py
@@ -1,112 +1,35 @@
-# Lift build system config script
 import os
-import sys
-import time
-import lift.print_color as Out
-from lift.files import Files
-from lift.compiler import Compiler
-from lift.workers import Workers
-from lift.incremental import FileModifiedCache
 
-# Toggle on/off printing of out.print_debug
-Out.TOGGLE_DEBUG = False
+from lift.lift import Lift
 
-project_path = os.getcwd()
-project_settings = {
-    "executable_name": "app",
-    # supported: "GCC", "CLANG"
-    "compiler": "CLANG",
-    "working_dir": project_path,
-    "path_src": project_path + "/src",
-    "build_path": project_path + "/build", 
-    }
+def build(lift):
+    # Project settings
+    lift.app_name = "app"
+    lift.dir_root = os.getcwd();
+    lift.dir_source = "/src"
+    lift.dir_build = "/build"
+    lift.dir_libs = ""
+    lift.dir_include = ""
+    lift.libs = ""
 
-# build mode can be RELEASE/DEBUG 
-def build(mode):
-    Out.print_debug("Running build()")
+    # Compiler settings
+    lift.compiler = "gcc"
+    lift.flags_debug = lift.flags_debug_default()
+    lift.flags_release = lift.flags_release_default()
 
-    ### Vars
-    which_compiler = project_settings["compiler"].lower()
-    executable_name =  project_settings.get("build_path") + "/" +  project_settings.get("executable_name")
+    # Incremental compilation settings
+    lift.incremental_compilation = True
 
-    ### Makind necessary dirs
-    if not os.path.exists(project_settings.get("build_path")):
-        os.mkdir(project_settings.get("build_path"))
-
-    ### Remove old executable if exist
-    # check if executable exist
-    if os.path.exists(executable_name):
-        # Remove the file
-        os.remove(executable_name)
-
-    ### Finding source files
-    Out.print_info("> Finding source files")
-    # get all files in path_src
-    path_src_files = Files(project_settings["path_src"])
-    # exclude files which are not *.h and *.c 
-    path_src_source = path_src_files.get_files_with_extensions({".h",".c"})
-    Out.print_debug(path_src_source)
-
-    ### Compiler setup
-    Out.print_info("> Compiler setup")
-    comp = Compiler(project_settings["compiler"])
-    flags = comp.generate_flags(mode)
-
-    ### mtime cache
-    Out.print_info("> Resolve incremental compilation")
-    cache = FileModifiedCache()
-    cache.load()
-    Out.print_debug(cache.mtime_cache)
-
-    for file in path_src_source:
-        mtime = os.path.getmtime(file)
-        if cache.mtime_cache.get(file):
-            if mtime > float(cache.mtime_cache[file]):
-                Out.print_color(Out.COLOR.BLUE, f'{file} has been modified since last compilation')
-        
-        cache.add_file(file) # update cache
+    # Compilation & Linker settings
+    lift.threads = 8
     
-    cache.store() # at end of compilation we should store back to disk
-    
-    ### Dependency graph
-    # TODO
+    return lift
 
-    ### Generating object files
-    Out.print_info("> Generating *.o files")
-    workers_gen_o = Workers()
-    o_jobs = []
-    for file in path_src_source:
-        if not file.endswith(".c"):
-            continue
-        working_dir = project_settings["working_dir"]
-        o_file_name = file.split("/")[-1].replace(".c", ".o")
-        o_file = f"{working_dir}/build/{o_file_name}"
-        o_jobs.append(f"{flags} -c '{file}' -o '{o_file}'")
-    workers_gen_o.work(which_compiler, o_jobs)
-
-    ### Linking into executable
-    Out.print_info("> Linking *.o files into executable")
-    workers_gen_exe = Workers()
-    o_files_dir = Files(project_settings["build_path"])
-    o_files_list = o_files_dir.get_files_with_extensions({".o"})
-    o_files = ""
-    for file in o_files_list:
-        o_files += f" '{file}' "
-    job = [f"{flags} {o_files} -o '{executable_name}'"]
-    workers_gen_exe.work(which_compiler, job)
 
 
 def run():
     Out.print_debug("Running run()")
-    # The name of the executable
-    executable =  project_settings.get("build_path") + "/" +  project_settings.get("executable_name")
-    # The arguments to pass to the executable
-    args = [""]
-    # Combine the executable name and arguments into a single list
-    exec_args = [executable] + args
-    # Replace the current process with the executable
-    if os.path.exists(executable):
-        os.execvp(executable, exec_args)
+
 
 def clean():
     Out.print_debug("Running clean()")
