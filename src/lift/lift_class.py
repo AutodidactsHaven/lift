0eb2541c997fbccffb6159c789599ecc1a6c6a34
Mihails M <mail@del.cx>
2023-02-13 14:59:14 +0000

    Back to working compilation and linking, fully usable with new lift_build.py, waiting on Omni

--- src/lift/lift_class.py
+++ src/lift/lift_class.py
@@ -2,6 +2,7 @@ import os
 import lift.print_color as Out
 from lift.compiler import Compiler
 from lift.files import Files
+from lift.workers import Workers
 
 class LiftClass:
     # Name of your app
@@ -43,6 +44,8 @@ class LiftClass:
         self.flags_release = self.flags_default("release")
         # TODO: Load preference for gcc/clang from config
 
+    ###########################################################################
+
     # "release" / "debug"
     def flags_default(self, mode):
         comp = Compiler(self.compiler)
@@ -50,10 +53,13 @@ class LiftClass:
 
     def build(self, mode):
         Out.print_debug(f"> Running LiftClass.build({mode})")
+        # compile object files
         source_files = self.get_source_files()
-        self.compile(mode, source_files)
-        object_files = self.get_object_files()
-        self.link(mode, object_files)
+        files_to_compile = self.get_files_filter(source_files, ".c"); # FIXME: Temporary until Omni is done
+        self.compile(mode, files_to_compile)
+        # link object files 
+        objects_to_link = self.get_object_files()
+        self.link(mode, objects_to_link)
 
     def run(self):
         Out.print_debug("> Running LiftClass.run()")
@@ -66,24 +72,66 @@ class LiftClass:
 
     def compile(self, mode, source_files):
         Out.print_debug(f"> Running LiftClass.compile({mode}, {source_files})")
+        self.dir_makedir(self.dir_root + self.dir_build)
+        self.dir_makedir(self.dir_root + self.dir_build + self.dir_object)
+        # generate compiler command
+        commands = []
+        for file in source_files:
+            o_file = self.dir_root + self.dir_build + self.dir_object + "/" + file.split("/")[-1].replace(".c", ".o")
+            command = f"{self.get_flags(mode)} -c '{file}' -o '{o_file}'"
+            commands.append(command);
+        # spawn workers 
+        worker_manager = Workers()
+        worker_manager.work(self.compiler, commands, self.threads)
+        
 
     def link(self, mode, object_files):
         Out.print_debug(f"> Running LiftClass.link({mode}, {object_files})")
+        self.dir_makedir(self.dir_root + self.dir_build)
+        self.dir_makedir(self.dir_root + self.dir_build + self.dir_object)
+        # Combine all objects into string
+        objects = ""
+        for file in object_files:
+            objects += f" '{file}' "
+        # generate compiler command
+        app_name = self.dir_root + self.dir_build + "/" + self.app_name
+        command = f"{self.dir_lib} {self.libs} {self.dir_include} {self.get_flags(mode)} {objects}  -o '{app_name}'"
+        # spawn workers 
+        worker_manager = Workers()
+        worker_manager.work(self.compiler, [command], self.threads)
 
-    def get_source_files(self):
-        Out.print_debug("> Running LiftClass.get_source_files()")
-        files = Files(self.dir_root + self.dir_source)
-        source_files = files.get_files_with_extensions({".c", ".h"})
+    def get_source_files(self):        
         # TODO: Scanning files for #includes
         # TODO: Generating dependency graph
         # TODO: Exluding elements in (file_blacklist, dir_blacklist) from files
+        Out.print_debug("> Running LiftClass.get_source_files()")
+        files = Files(self.dir_root + self.dir_source)
+        source_files = files.get_files_with_extensions({".c", ".h"})
         return source_files
 
     def get_object_files(self):
-        Out.print_debug("> Running LiftClass.get_object_files()")
-        files = Files(self.dir_root + self.dir_build + self.dir_object)
-        object_files = files.get_files_with_extensions({".o"})
         # TODO: Exluding elements in (file_blacklist, dir_blacklist) from .o files
         # if program was compiled eariles without those filters, these .o files still
         # might exist and become part of the executable.
+        Out.print_debug("> Running LiftClass.get_object_files()")
+        files = Files(self.dir_root + self.dir_build + self.dir_object)
+        object_files = files.get_files_with_extensions({".o"})
         return object_files 
+
+    def get_files_filter(self, files, extension):
+        file_list = []
+        for file in [f for f in files if f.endswith(extension)]:
+            file_list.append(file)
+        return file_list
+
+    def get_flags(self, mode):
+        if mode == "debug":
+            return self.flags_debug
+        else:
+            return self.flags_release
+
+    # make dir if it does not exist
+    def dir_makedir(self, directory):
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+
--- src/lift/workers.py
+++ src/lift/workers.py
@@ -19,16 +19,21 @@ class Workers:
         result = subprocess.run([app] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         return result.stdout, result.stderr
 
-    def work(self, app, jobs_list):
+    def work(self, app, jobs_list, thread_limit):
         threads = []
         Out.print_info(f"> - Total {len(jobs_list)} jobs")
+        done = 0
         for job in jobs_list:
+            if len(threads) >= thread_limit:
+                for t in threads:
+                    t.join()
+                threads = []
             t = Thread(target=self.internal_worker_capture, args=(app, job))
             threads.append(t)
             t.start()
-
-        done = 0
-        for t in threads:
-            t.join()
             done += 1
             Out.print_info(f"> - ↺  {done}/{len(jobs_list)} {int((done / len(jobs_list)) * 100 + 0.5)}%")
+
+        for t in threads:
+            t.join()
+        Out.print_info("> - All jobs finished")
